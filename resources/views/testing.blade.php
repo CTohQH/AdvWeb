f6e13eb4dfa796e5f18747dc1c609a59983a678e
CTohQH <qhst2003@gmail.com>
2025-03-22 18:44:55 +0800

    Initial upload to existing repository

---
+++ resources/views/testing.blade.php
@@ -0,0 +1,6 @@
+<html>
+
+<body>
+    {{ dd(session(),Auth::user()) }}
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/testing', function () {
+    return view('testing');
+});
+
+
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('login'); // Add a method to show the login form
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register'); // Add a method to show the registration form
+Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::get('/user/{id}', [UserController::class, 'getUserProfile'])->name('user.profile');
+
+Route::get('/homepage', function () {
+    return view('homepage'); // Ensure you have a homepage view
+})->name('homepage');
+
+
+
+Route::get('/test-session', function () {
+    session(['test' => 'Session is working!']);
+    return session('test');
+});
